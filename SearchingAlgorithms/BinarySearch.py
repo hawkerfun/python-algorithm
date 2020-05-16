42672fddbeeb1f3b11560b68ae4c3e89deb0d1ee
Sergei Khazov <SerKhazov@gmail.com>
2020-05-16 17:26:13 +0000

    first commit

---
+++ SearchingAlgorithms/BinarySearch.py
@@ -0,0 +1,17 @@
+def binarySearch(arr, target, left, right):
+  random_pos = 0
+  while True:
+    if left > right or random_pos == (right - left) // 2:
+      return -1
+    
+    random_pos = (right - left) // 2
+    if target > arr[random_pos]:
+      left = random_pos
+      continue
+
+    if target < arr[random_pos]:
+      right = random_pos
+      continue
+
+    if target == arr[random_pos]:
+      return random_pos
---
+++ main.py
@@ -0,0 +1,5 @@
+from SearchingAlgorithms.BinarySearch import binarySearch
+
+arr = [ 2, 3, 4, 10, 40 ]
+pos = binarySearch(arr, 55, 0, len(arr) - 1)
+print(pos)
